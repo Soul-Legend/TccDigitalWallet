fc2d0f357ccbbc6c5ab0647e15a45fe7980f57c7
Pedro Taglialenha <pedro.taglialenha@asml.com>
2026-04-22 21:52:06 +0200

    revert: remove font loading that caused white screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import {Stack} from 'expo-router';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {useFonts} from 'expo-font';
 
 /**
  * Root layout for the digital wallet app.
  */
 export default function RootLayout(): React.JSX.Element {
-  const [fontsLoaded] = useFonts({
-    MaterialIcons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.ttf'),
-    MaterialCommunityIcons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return <></>;
-  }
-
   return (
     <SafeAreaProvider>
       <Stack
